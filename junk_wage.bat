d9c7dfae2790084611d366f108837c4d8bc9c6d7
ruslan-ok <ruslan.ok@gmail.com>
2018-10-04 23:07:40 +0300

    Отчет "Повышения"

---
+++ junk_wage.bat
@@ -0,0 +1,3 @@
+rem D:\GalService\junction.exe D:\Gal\Src\Central\CompSrc\Z\Z_Wage\Fr3 D:\Gal\Src\Wage\Fr3
+D:\GalService\junction.exe -s D:\Gal\Src\Central\CompSrc\Z\Z_Wage\vip D:\Gal\Src\Wage\v2
+
